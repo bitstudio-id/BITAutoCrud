64b5e5cfad0f9431b82502acd7dffe77cad81e22
G9Insane <2202insane@gmail.com>
2019-08-16 08:23:20 +0700

    aplah

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,5 @@ const mix = require('laravel-mix');
 
 mix
     .js('resources/js/app.js', 'public/js')
-    .scripts('resources/js/bits.js','public/js/bits.js');
+    .scripts('resources/js/bits.js','public/js/bits.js')
     .sass('resources/sass/app.scss', 'public/css');
